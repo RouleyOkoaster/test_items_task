125b4e908511bb006b89d4fa22aa0bbefcc6402d
RouleyOkoaster <rouleyokoaster@gmail.com>
2026-05-18 20:13:55 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ def test_add_to_cart_button_is_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.implicitly_wait(10)
-    browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert True
+    button=browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(button)==1, "Button either isn't present or theres more than one with that selector"
